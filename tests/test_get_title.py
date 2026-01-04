3af438267f14fff162cc66f154857563a416a724
Sharon Caphona Devanayagam <sharondeva197@gmail.com>
2026-01-04 22:55:47 +0530

    day 13

---
+++ tests/test_get_title.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    #Open the browser in the headless mode
+    browser = p.chromium.launch(headless=False)
+
+    #Create a new browser context
+    context = browser.new_context()
+
+    #Create a new page in the browser context
+    page = context.new_page()
+
+    #Go the webpage
+    page.goto("www.facebook.com")
+    print(page.title())
+
+    #Close the browser
+    browser.close()
